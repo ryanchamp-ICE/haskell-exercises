d172bf47db2320b348a7e4fe63b2acfef032f538
Ryan Champ <ryan.champ@gmail.com>
2022-01-29 08:31:47 -0600

    Added file for S7 assignment

---
+++ S7-non-empty-list.hs
@@ -0,0 +1,13 @@
+-- data NonEmptyList a = ?
+
+-- nelToList :: NonEmptyList a -> [a]
+-- nelToList = ?
+
+-- listToNel :: [a] -> Maybe (NonEmptyList a)
+-- listToNel = ?
+
+-- headNEL :: NotEmptyList a -> a
+-- headNEL = ?
+
+-- tailNEL :: NonEmptyLis a -> [a]
+-- tailNEL
